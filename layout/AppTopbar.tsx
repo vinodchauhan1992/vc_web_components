d80bb4108dcfb079d3b362469c5d64ba3e26b955
vinodchauhan1992 <69452412+vinodchauhan1992@users.noreply.github.com>
2024-12-17 17:05:13 +0530

    updated

--- layout/AppTopbar.tsx
+++ layout/AppTopbar.tsx
@@ -1,9 +1,11 @@
 import { AppTopbarRef } from '@/types';
 import Link from 'next/link';
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { forwardRef, useContext, useImperativeHandle, useRef } from 'react';
+import { LayoutContext } from './context/layoutcontext';
 
 const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
   // REFS
+  const { onMenuToggle } = useContext(LayoutContext);
   const menubuttonRef = useRef(null);
   const topbarmenuRef = useRef(null);
   const topbarmenubuttonRef = useRef(null);
@@ -19,6 +21,14 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
       <Link href="/" className="layout-topbar-logo">
         <span>VC Components</span>
       </Link>
+      <button
+        ref={menubuttonRef}
+        type="button"
+        className="p-link layout-menu-button layout-topbar-button"
+        onClick={onMenuToggle}
+      >
+        <i className="pi pi-bars" />
+      </button>
     </div>
   );
 });
